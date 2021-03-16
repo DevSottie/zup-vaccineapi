dea06e994a2d7bc82d5a7cf4275a3e5dd94963ad
DevSottie <j.vitorsilva.dev@gmail.com>
2021-03-16 02:12:58 -0300

    first commit

---
+++ src/main/java/zup/orangetalents/vaccineapi/VaccineapiApplication.java
@@ -0,0 +1,13 @@
+package zup.orangetalents.vaccineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccineapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zup/orangetalents/vaccineapi/VaccineapiApplicationTests.java
@@ -0,0 +1,13 @@
+package zup.orangetalents.vaccineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccineapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
