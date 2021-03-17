e7359dd9da43b357dd0fdd2dc91f7c1051b7de90
DevSottie <j.vitorsilva.dev@gmail.com>
2021-03-17 02:47:02 -0300

    adicionada função de exibição do erro ao consumidor da api

--- src/main/java/zup/orangetalents/vaccineapi/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/zup/orangetalents/vaccineapi/exceptionhandler/ApiExceptionHandler.java
@@ -3,22 +3,36 @@ package zup.orangetalents.vaccineapi.exceptionhandler;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @ControllerAdvice //diz que essa classe é um componente do spring onde vão ser colocados os tratamentos das exceções de todos os controladores
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        var campos = new ArrayList<ExceptionHandled.Campo>();
+
+        for (ObjectError error : ex.getBindingResult().getAllErrors()){
+            String nome = ((FieldError) error).getField();
+            String mensagem = error.getDefaultMessage();
+
+            campos.add(new ExceptionHandled.Campo(nome, mensagem));
+        }
+
         var exception = new ExceptionHandled();
 
         exception.setStatus(status.value());
         exception.setTitle("Um ou mais campos inseridos estão inválidos");
         exception.setDataHora(LocalDateTime.now());
+        exception.setCampos(campos);
 
         return super.handleExceptionInternal(ex,exception, headers, status, request);
     }
--- src/main/java/zup/orangetalents/vaccineapi/exceptionhandler/ExceptionHandled.java
+++ src/main/java/zup/orangetalents/vaccineapi/exceptionhandler/ExceptionHandled.java
@@ -1,11 +1,40 @@
 package zup.orangetalents.vaccineapi.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ExceptionHandled {
     private Integer status;
     private LocalDateTime dataHora;
     private String title;
+    private List<Campo> campos;
+
+    public static class Campo{
+        private String nome ;
+        private String mensagem;
+
+        public Campo(String nome, String mensagem) {
+            super();
+            this.nome = nome;
+            this.mensagem = mensagem;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String getMensagem() {
+            return mensagem;
+        }
+
+        public void setMensagem(String mensagem) {
+            this.mensagem = mensagem;
+        }
+    }
 
     public Integer getStatus() {
         return status;
@@ -29,5 +58,13 @@ public class ExceptionHandled {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public List<Campo> getCampos() {
+        return campos;
+    }
+
+    public void setCampos(List<Campo> campos) {
+        this.campos = campos;
     }
 }
