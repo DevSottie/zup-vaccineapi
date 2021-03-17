c6af99c11c3d53406d3b898642c2c7be29bedd4d
DevSottie <j.vitorsilva.dev@gmail.com>
2021-03-16 23:36:58 -0300

    adicionado metodo de pesquisa de usuario por id

--- src/main/java/zup/orangetalents/vaccineapi/controller/UserController.java
+++ src/main/java/zup/orangetalents/vaccineapi/controller/UserController.java
@@ -2,6 +2,7 @@ package zup.orangetalents.vaccineapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import zup.orangetalents.vaccineapi.model.User;
 import zup.orangetalents.vaccineapi.repository.UserRepository;
@@ -9,6 +10,7 @@ import zup.orangetalents.vaccineapi.repository.UserRepository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -19,5 +21,13 @@ public class UserController {
     @GetMapping("/api/v1/user")
     public List<User> listUsers(){
         return userRepository.findAll();
+//        return userRepository.findByNome("Beatriz Daniela Laís Martins");
+//        return userRepository.findByNomeContaining("Beatriz");
+    }
+
+    @GetMapping("/api/v1/user/{userId}")
+    public User buscar(@PathVariable Long userId){
+        Optional<User> user =  userRepository.findById(userId);
+        return user.orElse(null);
     }
 }
