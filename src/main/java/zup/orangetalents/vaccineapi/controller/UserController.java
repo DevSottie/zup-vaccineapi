ac19301eee1272310404f421f1046d2203c786ef
DevSottie <j.vitorsilva.dev@gmail.com>
2021-03-17 00:05:32 -0300

    Adicionado retorno com status 404 quando o id não for encontrado

--- src/main/java/zup/orangetalents/vaccineapi/controller/UserController.java
+++ src/main/java/zup/orangetalents/vaccineapi/controller/UserController.java
@@ -1,33 +1,43 @@
 package zup.orangetalents.vaccineapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import zup.orangetalents.vaccineapi.model.User;
 import zup.orangetalents.vaccineapi.repository.UserRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/api/v1/user")
+    @GetMapping
     public List<User> listUsers(){
         return userRepository.findAll();
 //        return userRepository.findByNome("Beatriz Daniela Laís Martins");
 //        return userRepository.findByNomeContaining("Beatriz");
     }
 
-    @GetMapping("/api/v1/user/{userId}")
-    public User buscar(@PathVariable Long userId){
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> buscar(@PathVariable Long userId){
         Optional<User> user =  userRepository.findById(userId);
-        return user.orElse(null);
+
+        if(user.isPresent()){
+            return ResponseEntity.ok(user.get());
+//            return new ResponseEntity<User>(user.get(), HttpStatus.CREATED);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
