7966a5aadd45ba73102ae508664eb7029aad8c54
DevSottie <j.vitorsilva.dev@gmail.com>
2021-03-16 23:04:19 -0300

    função de listagem de usuarios adicionada

--- src/main/java/zup/orangetalents/vaccineapi/controller/UserController.java
+++ src/main/java/zup/orangetalents/vaccineapi/controller/UserController.java
@@ -1,2 +1,22 @@
-package zup.orangetalents.vaccineapi.controller;public class UserController {
+package zup.orangetalents.vaccineapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import zup.orangetalents.vaccineapi.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @GetMapping("/api/v1/user")
+    public List<User> listUsers(){
+        return manager.createQuery("from User", User.class)// createNamedQuery é do JPA(JPQL ao invés de sql)
+                .getResultList();
+    }
 }
--- src/main/java/zup/orangetalents/vaccineapi/model/User.java
+++ src/main/java/zup/orangetalents/vaccineapi/model/User.java
@@ -1,2 +1,78 @@
-package zup.orangetalents.vaccineapi.model;public class User {
+package zup.orangetalents.vaccineapi.model;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "user_nome")
+    private String nome;
+
+    @Column(name = "user_email")
+    private String email;
+
+    @Column(name = "user_cpf")
+    private String cpf;
+
+
+    @Column(name = "user_birthdate")
+    private Date birthDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
